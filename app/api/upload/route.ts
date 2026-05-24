552267792bb0f8f8cfc40afe2d9b304c1a26edf4
David Martins <SEU_EMAIL_GITHUB>
2026-05-24 01:14:22 -0400

    Fix cloudinary import

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import cloudinary from '../../lib/prisma'
+import cloudinary from '../../../lib/cloudinary'
 
 export async function POST(req: Request) {
   try {
