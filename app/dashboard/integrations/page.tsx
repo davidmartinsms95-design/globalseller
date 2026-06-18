0e421b2eb016891e848902452a26a38a3e5eed2a
David Martins <SEU_EMAIL_GITHUB>
2026-06-17 21:39:27 -0400

    teste botao integracao google

--- app/dashboard/integrations/page.tsx
+++ app/dashboard/integrations/page.tsx
@@ -2,9 +2,11 @@
 
 export default function IntegrationsPage() {
   async function connectMercadoLivre() {
-    window.location.href =
-      'https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=7497664436144745&redirect_uri=https%3A%2F%2Fglobalseller-zhun.vercel.app%2Fapi%2Fintegrations%2Fmercadolivre%2Fcallback'
-  }
+  alert('BOTÃO FUNCIONOU')
+
+  window.location.href =
+    'https://google.com'
+}
 
   return (
     <div>
